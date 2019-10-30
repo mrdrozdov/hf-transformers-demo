cb580c3ba4f4f6bf7926969a10a4a0fdc652427b
Andrew Drozdov <andrew@mrdrozdov.com>
2019-10-30 16:36:11 -0400

    Eval.

--- cluster.py
+++ cluster.py
@@ -3,11 +3,23 @@ import os
 import json
 import nltk
 
+from tqdm import tqdm
+
 import numpy as np
 from sklearn.cluster import KMeans, MiniBatchKMeans
 
 import torch
 from transformers import *
+
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--cuda', action='store_true')
+parser.add_argument('--random', action='store_true')
+parser.add_argument('--batch', action='store_true')
+parser.add_argument('--layer_choice', default=-1, type=int)
+options = parser.parse_args()
 
 
 print('Load model.')
@@ -54,15 +66,14 @@ pretrained_weights = 'spanbert_hf_base'
 tokenizer = BertTokenizer.from_pretrained(pretrained_weights)
 model_class = BERT_MODEL_CLASSES[0]
 
-# Load pretrained model/tokenizer
-model = model_class.from_pretrained(pretrained_weights)
-
 # Models can return full list of hidden-states & attentions weights at each layer
 model = model_class.from_pretrained(pretrained_weights,
                                     output_hidden_states=True,
                                     output_attentions=True)
 #input_ids = torch.tensor([tokenizer.encode("Let's see all hidden-states and attentions on this text")])
 #all_hidden_states, all_attentions = model(input_ids)[-2:]
+if options.cuda:
+    model.cuda()
 
 
 # Read data.
@@ -117,12 +128,12 @@ with open('22.auto.clean') as f:
         tokens = nltk_tree.leaves()
         rich_labeled_spans = get_spans(nltk_tree)
         labeled_spans = [(pos, size, labels[0]) for pos, size, labels in rich_labeled_spans]
-        input_ids = tokenizer.encode(' '.join(tokens))
+        input_ids = tokenizer.encode(' '.join(tokens), add_special_tokens=True)
         subword_units = [tokenizer.encode(x) for x in tokens]
         subword_ids = flatten(subword_units)
         offsets = subword_offsets(subword_units)
         assert len(offsets) == len(tokens)
-        assert tuple(input_ids) == tuple(subword_ids)
+        assert tuple(input_ids[1:-1]) == tuple(subword_ids)
         dataset.append(dict(labeled_spans=labeled_spans, tokens=tokens, input_ids=input_ids, offsets=offsets))
 
 # Encode.
@@ -132,7 +143,7 @@ print('Encode.')
 ## Sort by token length.
 dataset = sorted(dataset, key=lambda x: len(x['input_ids']))
 
-max_batch_size = 32
+max_batch_size = 128
 
 def batch_iterator(dataset):
     batch = []
@@ -155,23 +166,64 @@ def batch_iterator(dataset):
     if len(batch) > 0:
         yield batch
 
-layer_choice = -1
+layer_choice = options.layer_choice
 example_ids = []
 vectors = []
-for i, batch in enumerate(batch_iterator(dataset)):
+labels = []
+for i, batch in tqdm(enumerate(batch_iterator(dataset)), disable=False):
     input_ids = torch.tensor([x['input_ids'] for x in batch])
+    if options.cuda:
+        input_ids = input_ids.cuda()
     batch_size, length = input_ids.shape
 
     # Encode.
     all_hidden_states, all_attentions = model(input_ids)[-2:]
-    h = all_hidden_states[layer_choice].view(batch_size*length, -1)
-    vectors.append(h.cpu().data.numpy())
+    size = all_hidden_states[0].shape[-1]
+    h = all_hidden_states[layer_choice].view(batch_size, length, size)
+    if options.random:
+        h = h.normal_()
+    h = h.cpu().data.numpy()
+    #h = all_hidden_states[layer_choice].view(batch_size*length, -1)
+    #vectors.append(h.cpu().data.numpy())
 
     # Save example ids.
-    batch_example_ids = torch.LongTensor(batch_size, length)
-    for j in range(batch_size):
-        batch_example_ids[j] = i * batch_size + j
-    example_ids.append(batch_example_ids.view(-1))
+    #batch_example_ids = torch.LongTensor(batch_size, length)
+    #for j in range(batch_size):
+    #    batch_example_ids[j] = i * batch_size + j
+    #example_ids.append(batch_example_ids.view(-1))
+
+    # All phrases.
+    batch_example_ids = []
+    batch_labels = []
+    batch_vec = []
+    for j, subbatch in enumerate(batch):
+        offsets = subbatch['offsets']
+        labeled_spans = subbatch['labeled_spans']
+        for pos, size, label in labeled_spans:
+            if size < 2:
+                continue
+            special_offset = 1
+            start = special_offset + offsets[pos][0]
+            end = special_offset + offsets[pos + size - 1][0] + offsets[pos + size - 1][1]
+            boundary_start = h[j, start - 1]
+            boundary_end = h[j, end]
+            internal = np.mean(h[j, start:end], axis=0)
+            vec = np.concatenate([boundary_start, boundary_end, internal], axis=-1).reshape(1, 768 * 3)
+            batch_example_ids.append(i * batch_size + j)
+            batch_labels.append(label)
+            batch_vec.append(vec)
+
+    example_ids.append(batch_example_ids)
+    vectors.append(batch_vec)
+    labels.append(batch_labels)
+
+example_ids = flatten(example_ids)
+vectors = flatten(vectors)
+labels = flatten(labels)
+
+print('# of phrases', len(example_ids))
+assert len(example_ids) == len(vectors)
+assert len(example_ids) == len(labels)
 
 # Cluster.
 
@@ -182,7 +234,36 @@ seed = 11
 
 X = np.concatenate(vectors, axis=0)
 algo = MiniBatchKMeans(n_clusters=n_clusters, random_state=seed, batch_size=1600)
+#algo = KMeans(n_clusters=n_clusters, random_state=seed, n_jobs=8, verbose=1)
 algo.fit(X)
 
 cluster_ids = algo.labels_
+
+import collections
+
+summary = collections.OrderedDict()
+
+for idx in range(n_clusters):
+    summary[idx] = collections.Counter()
+
+for idx, y in zip(cluster_ids, labels):
+    summary[idx][y] += 1
+
+total_correct = 0
+total_n = 0
+for idx, counter in summary.items():
+    if len(counter) == 0:
+        print('cluster = {}, skipped'.format(idx))
+    n = sum(counter.values())
+    most_common = counter.most_common(3)
+    correct = most_common[0][1]
+    correct_label = most_common[0][0]
+
+    print('cluster = {} {} {}/{}, {}'.format(
+        idx, correct_label, correct, n, most_common))
+
+    total_correct += correct
+    total_n += n
+
+print('Accuracy = {:.3f} {}/{}'.format(total_correct/total_n, total_correct, total_n))
 
